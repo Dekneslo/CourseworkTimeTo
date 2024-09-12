1e4f7d51b32750c744edfbd9458db4f68936630c
TiamatBronze1 <95623386+TiamatBronze1@users.noreply.github.com>
2024-09-12 16:15:37 +0300

    upd project

---
+++ Task_3/DataAccess/Repositories/RepositoryBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using DataAccess.Models;
+using System.Linq.Expressions;
+using DataAccess.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+namespace DataAccess.Repositories
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected /*df*/ RepositoryContext { get; set; }
+        public RepositoryBase(/*df*/ repositoryContext)
+        {
+            RepositoryContext = repositoryContext;
+        }
+        public IQueryable<T> FindAll() => RepositoryContext.Set<T>().AsNoTracking();
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression) => RepositoryContext.Set<T>().Where(expression).AsNoTracking();
+        public void Create(T entity) => RepositoryContext.Set<T>().Add(entity);
+        public void Update(T entity) => RepositoryContext.Set<T>().Update(entity);
+        public void Delete(T entity) => RepositoryContext.Set<T>().Delete(entity);
+    }
+}
---
+++ Task_3/DataAccess/Wrapper/RepositoryWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using DataAccess.Models;
+
+namespace DataAccess.Wrapper
+{
+    public class RepositoryWrapper : IRepositoryWrapper
+    {
+        private /*dd*/ _repoContext;
+        public RepositoryWrapper(/*dd*/ repositoryContext)
+        {
+            _repoContext = repositoryContext;
+        }
+        public void Save()
+        {
+            _repoContext.SaveChanges();
+        }
+    }
+}
