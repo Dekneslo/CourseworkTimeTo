6d8880baa7f14fbfee49abfaebb9ef356a9b3e74
Dekneslo <106800978+Dekneslo@users.noreply.github.com>
2024-10-31 20:36:07 +0300

    //

--- Api_Kim/project/obj/Debug/net6.0/project.AssemblyInfo.cs
+++ Api_Kim/project/obj/Debug/net6.0/project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e0e12e0654eedeecacebf45798c565eb04166b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a93c4b55c2a8987bca7065df55598a2d8bb5a740")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
